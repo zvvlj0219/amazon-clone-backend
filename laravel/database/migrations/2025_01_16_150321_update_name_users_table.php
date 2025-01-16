1700262c4f317cec42f9563abf858725e0d29bb7
zvvlj0219 <s00302448@gmail.com>
2025-01-17 00:19:13 +0900

    add columns

---
+++ laravel/database/migrations/2025_01_16_150321_update_name_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('first_name')->after('id')->comment('苗字');
+            $table->string('last_name')->after('first_name')->comment('名前');
+
+            $table->dropColumn('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name');
+
+            $table->dropColumn('first_name');
+            $table->dropColumn('last_name');
+        });
+    }
+};
